94a258d52bf216b92e7596d347f990b921007dda
Fonseca Naranjo Carlos <Carlos.Fonseca+Suracl@externos.afpcapital.cl>
2025-06-10 12:07:58 -0500

    Se aplican correcciones generales

--- src/app/components/projects.tsx
+++ src/app/components/projects.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import Image from "next/image";
+
 import ProjectAccordion from "./projectAccordion";
 
 import { porjectList } from "@/data/projectList";
@@ -12,21 +14,21 @@ export default function Projects() {
       <div className="w-full flex justify-center mt-12">
         {/* frontend */}
         <div className="flex justify-center mx-6">
-          <img alt="Angular" className="mx-2" width="40" src="icons/angular.svg" />
-          <img alt="React" className="mx-2" width="40" src="icons/react.svg" />
-          <img alt="Next" className="mx-2" width="40" src="icons/next.svg" />
-          <img alt="Vue" className="mx-2" width="40" src="icons/vue.svg" />
+          <Image alt="Angular" className="mx-2" height={40} src="/icons/angular.svg" width={40} />
+          <Image alt="React" className="mx-2" height={40} src="/icons/react.svg" width={40} />
+          <Image alt="Next" className="mx-2" height={40} src="/icons/next.svg" width={40} />
+          <Image alt="Vue" className="mx-2" height={40} src="/icons/vue.svg" width={40} />
         </div>
 
         {/* mobile */}
         <div className="flex justify-center mx-6">
-          <img alt="Ionic" className="mx-2" width="40" src="icons/ionic.svg" />
+          <Image alt="Ionic" className="mx-2" height={40} src="/icons/ionic.svg" width={40} />
         </div>
 
         {/* backend */}
         <div className="flex justify-center mx-6">
-          <img alt="Node" className="mx-2" width="40" src="icons/node.svg" />
-          <img alt="Nest" className="mx-2" width="40" src="icons/nest.svg" />
+          <Image alt="Node" className="mx-2" height={40} src="/icons/node.svg" width={40} />
+          <Image alt="Nest" className="mx-2" height={40} src="/icons/nest.svg" width={40} />
         </div>
       </div>
 
